21c6e18c4beb20f5a5d37ab3aec5c4e47abc1b3b
emanuel sobral <emanuelssobral@gmail.com>
2023-09-14 10:41:53 -0300

    funcao adicionada no botao C e =

--- main.js
+++ main.js
@@ -10,11 +10,27 @@ const allowedKeys = ["(",")", "", "", "-", "+", "9", "8", "7", "6", "5", "4", "3
 document.querySelectorAll('.charKey').forEach(function (charKeyButton) {
     // Para cada botão, adiciona um ouvinte de evento que é acionado quando o botão é clicado
     charKeyButton.addEventListener('click', function() {
+        // Foca no input ao botao ser clicado
+        input.focus()
         // Quando o botão é clicado, adiciona o valor do atributo 'data-value' do botão ao valor atual do campo de entrada
         input.value += charKeyButton.dataset.value
         // Adiciona o valor da variável 'value' ao valor atual do campo de entrada
         input.value += value
     })
+})
+
+// Seleciona o botao com a classe 'clear' na página
+document.getElementById('clear').addEventListener('click', function() {
+    // Quando o botão é clicado, limpa o campo input
+    input.value = ''
+    // Foca no input ao botao ser clicado
+    input.focus()
+})
+
+// Seleciona o botao  com a classe 'equal' na página
+document.getElementById('equal').addEventListener('click', function() {
+    // Quando o botão é clicado, inicia a funcao
+    calculate()
 })
 
 // Adiciona um ouvinte de evento ao campo de entrada que é acionado quando uma tecla é pressionada
@@ -44,5 +60,5 @@ input.addEventListener('keydown', function(e) {
 
 // Funcao para calcular
 function calculate() {
-
+    console.log('Passou')
 }
